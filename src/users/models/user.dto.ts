e9288808fc9c64d14dfbfebc6bec05caffe8b0d5
Oksana Iziumchenko <oxanas@wizardsdev.com>
2021-07-09 11:46:40 +0300

    get user by id and get all users requests made

--- src/users/models/user.dto.ts
+++ src/users/models/user.dto.ts
@@ -3,17 +3,10 @@ import { IUser } from "./user.interface";
 
 export class UserDTO implements IUser{
     id: number;
-
     first_name: string;
-
     last_name: string;
-
     email: string;
-
     password: string;
-
     created_at: Date;
-
-    updated_at: Date;
-    
+    updated_at: Date;  
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -4,6 +4,7 @@ import { UserEntity } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserDTO } from './models/user.dto';
 import { IUser } from './models/user.interface';
+import { isBuffer } from 'util';
 
 @Injectable()
 export class UserService {
@@ -13,7 +14,33 @@ export class UserService {
     ){}
 
 
-    async createPost(user:UserDTO):Promise<IUser> {
+    async createUser(user:UserDTO):Promise<IUser> {
         return this.usersRepository.save(user);
     }
+
+    async readUser(id:number):Promise<IUser>{
+        console.log(id);
+        try{
+            return this.usersRepository.findOne(id);
+        }
+        catch{
+            let usernull:UserDTO;
+            return usernull;
+        }
+    }
+
+    async readAllUsers():Promise<IUser[]>{
+        try {
+            return this.usersRepository.find();
+        }
+        catch{
+            
+        }
+
+    }
+    
+
+    //async updateUser(id:string):Promise<IUser>{
+      //  return this.usersRepository.update();
+    //}
 }

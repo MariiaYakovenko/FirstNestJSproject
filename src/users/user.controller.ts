d322b897621d63bd3d4368c3d91a8da6221fe5d7
Oksana Iziumchenko <oxanas@wizardsdev.com>
2021-07-09 11:45:00 +0300

    get user by id and get all users requests made

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, HttpCode, Body } from '@nestjs/common';
+import { Controller, Post, HttpCode, Body, Put, Param, Get, HttpException, BadRequestException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './models/user.dto';
 import { IUser } from './models/user.interface';
@@ -6,11 +6,37 @@ import { IUser } from './models/user.interface';
 @Controller('users')
 export class UserController {
     constructor(private userService:UserService){}
+
     @Post()
     async create(@Body() user:UserDTO): Promise<IUser> {
         //console.log(`userController: ${user}`);
-        return this.userService.createPost(user);
+        return this.userService.createUser(user);
     }
+
+    @Get(':id')
+    async read(@Param('id') id:number):Promise<IUser>{
+        //console.log(id);
+        let user=this.userService.readUser(id);
+        if(user!=null){
+            return user;
+        }
+        else{
+            throw new BadRequestException('can\'t find the user');
+        }
+    }
+
+    @Get('all')
+    async readAll():Promise<IUser[]>{
+        console.log('bla');
+        return this.userService.readAllUsers();
+    }
+
+   // @Put(':id')
+    //async update(@Param('id') id:string):Promise<IUser>{
+
+    //    return this.userService.updateUser(id);
+    //}
+
     
    
 }
